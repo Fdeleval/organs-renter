537d3d88791f8cc5e63fd99dbeec68d3fcb3233b
Romain Borremans <romain.borremans@gmail.com>
2020-11-10 14:49:24 +0100

    added photo attribute to user with cloudinary and active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_many_attached :photos
+  has_one_attached :photo
   has_many :organs
   has_many :bookings
 

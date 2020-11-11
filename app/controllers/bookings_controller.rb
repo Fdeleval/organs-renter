aa0c035382bd903c907dffeb5d813f853ba08b94
pierre crochet <pierrecrochet03@gmail.com>
2020-11-11 16:17:42 +0100

    just correct bugs

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,16 @@
 class BookingsController < ApplicationController
-  def show
+  before_action :authenticate_user!
+
+  def index
     @user = current_user
     @bookings = Booking.where(user_id: @user.id)
     @email_name = split_string(@user.email)
-
-
   end
 
 
   def split_string(email_adress)
 
     i = email_adress.index('@')
-    email_adress[0...i]
+    email_adress[0...i].capitalize
   end
 end

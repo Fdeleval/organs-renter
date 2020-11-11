effe7967f877394aecf18fe642619f5a0d000a4e
Romain Borremans <romain.borremans@gmail.com>
2020-11-11 16:42:48 +0100

    new branch

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,6 @@ Organ.create!({organ_type: "lungs", description: "Very bad condition", user_id: 
 Organ.create!({organ_type: "lungs", description: "Very good condition", user_id: 4, available: true, price: 4})
 Organ.create!({organ_type: "brain", description: "good condition", user_id: 4, available: false, price: 4})
 
-
 Booking.create!({ organ_id: 4, user_id: 1, date_start: DateTime.new(2020, 11, 10, 22, 35, 0), date_end: DateTime.new(2020, 11, 18, 22, 35, 0)})
 Booking.create!({ organ_id: 1, user_id: 2, date_start: DateTime.new(2020, 11, 25, 22, 35, 0), date_end: DateTime.new(2020, 11, 30, 22, 35, 0)})
 Booking.create!({ organ_id: 2, user_id: 3, date_start: DateTime.new(2020, 11, 25, 22, 35, 0), date_end: DateTime.new(2020, 11, 30, 22, 35, 0)})
